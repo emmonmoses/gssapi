12f0a44c383faf6a060ccc21c259bc87884ecf04
Emmon Moses <emmononyango@gmail.com>
2022-02-12 16:18:27 +0300

    first commit
    gssapi first commit

---
+++ gssManRegistration/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace gssManRegistration.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ gssManRegistration/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gssManRegistration.WebApiApplication" Language="C#" %>
