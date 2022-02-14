ac655217dec87ae0852f884bd3958d9dfde39ec3
Emmon Moses <emmononyango@gmail.com>
2022-02-14 18:44:06 +0300

    Delete gssManRegistration directory

--- gssManRegistration/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace gssManRegistration.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- gssManRegistration/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace gssManRegistration.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
